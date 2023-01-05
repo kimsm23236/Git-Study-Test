3a58c7213fb47348dda387c6813b493f025f06fa
kimsm23236 <kimsm23236@gmail.com>
2023-01-05 09:35:36 +0900

    test

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,7 +6,9 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            // 오류가 날 일이 없는 포인트를 저장해 놓는것이 중요
             Console.WriteLine("Hello, World!");
+            // 바꿈
         }
     }
 }
