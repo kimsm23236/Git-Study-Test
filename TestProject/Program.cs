8763689045918cc567458e3fd72e227ff1212a60
kimsm23236 <kimsm23236@gmail.com>
2023-01-05 11:09:18 +0900

    [Beta] feat(Main) Add new feature -> user input

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,10 +7,20 @@ namespace TestProject
         static void Main(string[] args)
         {
             // 오류가 날 일이 없는 포인트를 저장해 놓는것이 중요
-            Console.WriteLine("Hello, World!");
+
             // 바꿈
 
-            Ruler ruler = new Ruler(10);
+            // { 2023.01.05. Add new feature user input
+            string userInput = string.Empty;
+            Console.WriteLine("This program convert Cm to Inch");
+            Console.Write("Input Cm Value : ");
+            userInput = Console.ReadLine();
+
+            int cmInput = 0;
+            int.TryParse(userInput, out cmInput);
+
+            Ruler ruler = new Ruler(cmInput);
+            // } 2023.01.05. Add new feature user input
             ruler.Run();
         }
     }
