4e6ae87a61b7c20659d512f9b0bbeb4b1ecf06e1
kimsm23236 <kimsm23236@gmail.com>
2023-01-05 09:45:33 +0900

    Ruler Project Setup

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -9,6 +9,29 @@ namespace TestProject
             // 오류가 날 일이 없는 포인트를 저장해 놓는것이 중요
             Console.WriteLine("Hello, World!");
             // 바꿈
+
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+        public Ruler(int cmValue)
+        {
+            Centimeter = cmValue;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {Inch}inch 입니다.");
         }
     }
 }
