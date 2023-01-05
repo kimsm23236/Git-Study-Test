533e361ea47ddc1a582796993ea3357820e28039
kimsm23236 <kimsm23236@gmail.com>
2023-01-05 12:07:03 +0900

    Merge Modified

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -44,19 +44,4 @@ namespace TestProject
             Console.WriteLine($"{this.Centimeter}cm 는 {Inch}inch 입니다.");
         }
     }
-
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            // 오류가 날 일이 없는 포인트를 저장해 놓는것이 중요
-            Console.WriteLine("Hello, World!");
-            // 바꿈
-
-            Ruler ruler = new Ruler(10);
-            ruler.Run();
-        }
-    }
-
-    
 }
